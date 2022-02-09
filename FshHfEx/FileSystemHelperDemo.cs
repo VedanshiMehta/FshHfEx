1f471aab334163aa7824ea1cd1942507a4f6736c
vedanshi.mehta <vedanshi.mehta@1R-76>
2022-02-09 16:30:41 +0530

    Add project files.

---
+++ FshHfEx/FileSystemHelperDemo.cs
@@ -0,0 +1,54 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Xamarin.Essentials;
+
+namespace FshHfEx
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme")]
+    public class FileSystemHelperDemo : Activity
+    {
+        private Button myButton;
+        private TextView myText;
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            SetContentView(Resource.Layout.fshDemo);
+
+            myButton = FindViewById<Button>(Resource.Id.buttonFHP);
+            myText = FindViewById<TextView>(Resource.Id.textViewFHP);
+
+            myButton.Click += MyButton_Click;
+        }
+
+        [Obsolete]
+        private async void MyButton_Click(object sender, EventArgs e)
+        {
+            const string filename = "Shin.txt";
+            using (var stream = await FileSystem.OpenAppPackageFileAsync(filename)) 
+            {
+                using (var reader = new StreamReader(stream)) 
+                {
+                    myText.Text = await reader.ReadToEndAsync();
+                }
+            
+            }
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
---
+++ FshHfEx/MainActivity.cs
@@ -0,0 +1,48 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Widget;
+using AndroidX.AppCompat.App;
+
+namespace FshHfEx
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        private Button bFsh;
+        private Button bHf;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            bFsh = FindViewById<Button>(Resource.Id.button1);
+            bHf = FindViewById<Button>(Resource.Id.button2);
+
+            bFsh.Click += BFsh_Click;
+            bHf.Click += BHf_Click;
+        }
+
+        private void BHf_Click(object sender, System.EventArgs e)
+        {
+            Intent j = new Intent(this, typeof(HapticFeedbackDemo));
+            StartActivity(j);
+        }
+
+        private void BFsh_Click(object sender, System.EventArgs e)
+        {
+            Intent i = new Intent(this, typeof(FileSystemHelperDemo));
+            StartActivity(i);
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+}
